3dc38de964b5ea0f8915da823dea6c48ba6baa7a
Andrew Burns <aburnsni@gmail.com>
2015-06-01 20:21:44 +0100

    Javascript resize page top padding to take account of various navbar heights.

--- js/mytheme.js
+++ js/mytheme.js
@@ -1,7 +1,19 @@
-jQuery(window).resize(function () { 
-    jQuery('body').css('padding-top', parseInt(jQuery('#main-navbar').css("height"))+10);
+jQuery(window).resize(function () {
+//jQuery('body').prepend('<div>'+jQuery(window).width()+'</div>');
+  if (parseInt(jQuery(window).width()) < 768) {
+    jQuery('body').removeAttr("style");
+  } else {
+    var x = "padding-top: " + (parseInt(jQuery('#main-navbar').height()) + 10) + "px !important";
+    jQuery('body').attr("style", x);
+  }
 });
 
-jQuery(window).load(function () { 
-    jQuery('body').css('padding-top', parseInt(jQuery('#main-navbar').css("height"))+10);        
+jQuery(window).load(function () {
+  if (parseInt(jQuery(window).width()) < 768) {
+    jQuery('body').removeAttr("style");
+  } else {
+    var x = "padding-top: " + (parseInt(jQuery('#main-navbar').height()) + 10) + "px !important";
+    jQuery('body').attr("style", x);
+  }
 });
+
