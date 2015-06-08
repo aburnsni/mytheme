e488b956c027b61b0f8fd5839eed0a9dd23196ac
Andrew Burns <aburnsni@gmail.com>
2015-06-08 14:58:43 +0100

    Styling and block for secondary vertical menu

---
+++ templates/menu-block-wrapper.tpl.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @file
+ * Default theme implementation to wrap menu blocks.
+ *
+ * Available variables:
+ * - $content: The renderable array containing the menu.
+ * - $classes: A string containing the CSS classes for the DIV tag. Includes:
+ *   menu-block-DELTA, menu-name-NAME, parent-mlid-MLID, and menu-level-LEVEL.
+ * - $classes_array: An array containing each of the CSS classes.
+ *
+ * The following variables are provided for contextual information.
+ * - $delta: (string) The menu_block's block delta.
+ * - $config: An array of the block's configuration settings. Includes
+ *   menu_name, parent_mlid, title_link, admin_title, level, follow, depth,
+ *   expanded, and sort.
+ *
+ * @see template_preprocess_menu_block_wrapper()
+ */
+?>
+<div class="sidebar-nav">
+  <div class="navbar navbar-default" role="navigation">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-navbar-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <span class="visible-xs navbar-brand">Sidebar menu</span>
+    </div>
+    <div class="<?php print $classes; ?> navbar-collapse collapse sidebar-navbar-collapse">
+      <?php print render($content); ?>
+    </div>
+  </div>
+</div>
