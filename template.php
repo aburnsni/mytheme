a9d76908da05c46858e835893defc7c66e4dc1bd
Andrew Burns <aburnsni@gmail.com>
2015-06-12 22:50:22 +0100

    Gmaps loading on front page and contact only

--- template.php
+++ template.php
@@ -150,3 +150,21 @@ function fleming_preprocess_image(&$variables) {
         }
     }
 }
+
+
+// Add gmaps js to front page
+
+function fleming_preprocess_page(&$vars)
+{
+    dpm (arg(0));
+  if($vars['is_front']) {
+    drupal_add_js('https://maps.googleapis.com/maps/api/js?v=3.exp');
+    drupal_add_js(drupal_get_path('theme', 'fleming') . '/js/gmap-block.js');
+  }
+  else if(arg(0)=='contact_us') {
+    drupal_add_js('https://maps.googleapis.com/maps/api/js?v=3.exp');
+    drupal_add_js(drupal_get_path('theme', 'fleming') . '/js/gmap-large.js');
+  }
+}
+
+
