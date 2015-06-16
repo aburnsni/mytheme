3ab9dcedeed214bbca6f8ccfb4464307cff5a820
Andrew Burns <aburnsni@gmail.com>
2015-06-16 10:26:58 +0100

    Revert "GMap fixes"
    This reverts commit 54a1fd14ca9a3cc33c68e1700a6c1867be37d617.

--- js/gmap-large.js
+++ js/gmap-large.js
@@ -1,12 +1,10 @@
-var myLatlng = new google.maps.LatLng(54.55663, -5.964576),
-    directionsDisplay,
-    directionsService = new google.maps.DirectionsService();
+var myLatlng = new google.maps.LatLng(54.55663, -5.964576);
 
 
 function initialize() {
     "use strict";
     var saveWidget,
-
+        
         mapOptions = {
             zoom: 15,
             center: myLatlng,
@@ -22,9 +20,9 @@ function initialize() {
                 position: google.maps.ControlPosition.TOP_LEFT
             }
         },
-
+        
         map = new google.maps.Map(document.getElementById('map-canvas-large'), mapOptions),
-
+        
         saveWidgetOptions = {
             place: {
                 // ChIJJfVFI6oIYUgRdw0d7GHwHOs is the place Id for FFS. Found using 
@@ -37,18 +35,18 @@ function initialize() {
                 webUrl: 'http://www.flemingfulton.org.uk'
             }
         },
-
+        
         contentString = '<div id="save-widget">' +
-        '<h4>Fleming Fulton School</h4>' +
-        '<div>' +
-        'Upper Malone Road<br />Belfast<br />BT9 6TY' +
-        '</div>' +
-        '</div>',
+            '<h4>Fleming Fulton School</h4>' +
+            '<div>' +
+            'Upper Malone Road<br />Belfast<br />BT9 6TY' +
+            '</div>' +
+            '</div>',
 
         infowindow = new google.maps.InfoWindow({
             content: contentString
         }),
-
+        
         marker = new google.maps.Marker({
             icon: '/sites/all/themes/fleming/logo.png',
             position: saveWidgetOptions.place.location,
@@ -62,43 +60,6 @@ function initialize() {
         var widgetDiv = document.getElementById('save-widget');
         saveWidget = new google.maps.SaveWidget(widgetDiv, saveWidgetOptions);
     });
-    
-    directionsDisplay = new google.maps.DirectionsRenderer();
-    directionsDisplay.setMap(map);
-    directionsDisplay.setPanel(document.getElementById("directionsPanel"));
 }
 
-function calcRoute() {
-    "use strict";
-    var start = document.getElementById("routeStart").value;
-    var end = myLatlng;
-    var request = {
-        origin: start,
-        destination: end,
-        travelMode: google.maps.DirectionsTravelMode.DRIVING
-    };
-    directionsService.route(request, function (response, status) {
-        if (status == 'ZERO_RESULTS') {
-            alert('No route could be found between the origin and destination.');
-        } else if (status == 'UNKNOWN_ERROR') {
-            alert('A directions request could not be processed due to a server error. The request may succeed if you try again.');
-        } else if (status == 'REQUEST_DENIED') {
-            alert('This webpage is not allowed to use the directions service.');
-        } else if (status == 'OVER_QUERY_LIMIT') {
-            alert('The webpage has gone over the requests limit in too short a period of time.');
-        } else if (status == 'NOT_FOUND') {
-            alert('At least one of the origin, destination, or waypoints could not be geocoded.');
-        } else if (status == 'INVALID_REQUEST') {
-            alert('The DirectionsRequest provided was invalid.');
-        } else if (status == google.maps.DirectionsStatus.OK) {
-            directionsDisplay.setDirections(response);
-        } else {
-            if (status == 'ZERO_RESULTS') {
-                alert("Could not calculate a route to or from one of your destinations.");
-            } else {
-                alert("There was an unknown error in your request. Requeststatus: " + status);
-            }
-        }
-    });
-}
 google.maps.event.addDomListener(window, 'load', initialize);
