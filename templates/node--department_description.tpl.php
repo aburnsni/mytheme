f646a8fd39bfc4390878120048f106b8e4b58d05
Andrew Burns <aburnsni@gmail.com>
2015-06-23 15:39:58 +0100

    Search result views

--- templates/node--department_description.tpl.php
+++ templates/node--department_description.tpl.php
@@ -79,7 +79,14 @@
  *
  * @ingroup themeable
  */
+global $base_url;
 ?>
+<?php if(user_access('edit any department_description content') || user_access('edit own department_description content')): ?>
+     <ul class="tabs--primary nav nav-tabs">
+        <li class="active"><a href="<?php print $base_url; ?>/taxonomy/term/<?php print $field_department['und'][0]['tid']; ?>" role="button">View Class<span class="element-invisible">(active tab)</span></a></li>
+        <li class=""><a href="<?php print $base_url; ?>/node/<?php print $node->nid; ?>/edit" role="button">Edit</a></li>
+    </ul>
+<?php endif; ?>
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
   <?php print $user_picture; ?>
