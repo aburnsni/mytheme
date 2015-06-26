e1485d531e7dfdd1cd6b9163a316c5f40a381a39
Andrew Burns <aburnsni@gmail.com>
2015-06-26 13:46:26 +0100

    Fix IE8/9 also add respondjs module

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -48,6 +48,7 @@
 <head <?php print $rdf_namespaces; ?>>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
