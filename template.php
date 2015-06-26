626d9989a2fe1d3d5258629448d18e62e60b1ed2
Andrew Burns <aburnsni@gmail.com>
2015-06-26 22:12:01 +0100

    Themed department taxonomy as buttons

--- template.php
+++ template.php
@@ -179,3 +179,34 @@ function fleming_form_alter(&$form, &$form_state, $form_id) {
 //    print '</pre>';
 //    $form['keys_1']['#attributes']['placeholder'] = "Search...";
 }
+
+/*
+** Alter rendering of taxonomt terms
+*/
+/**
+ * Implements theme_field__field_tags().
+ */
+function fleming_field__field_department(&$variables) {
+  $output = '';
+
+  // Render the label if it's not hidden.
+  if (!$variables['label_hidden']) {
+    $output .= '<div class="field-label"' . $variables['title_attributes'] . '>' . $variables['label'] . ':&nbsp;</div>';
+  }
+
+  // Render the items.
+  $index = 0;
+  $output .= '<div class="field-items"' . $variables['content_attributes'] . '>';
+  foreach ($variables['items'] as $delta => $item) {
+    $classes = 'field-item ' . ($delta % 2 ? 'odd' : 'even') . (!$index ? ' first' : '');
+    $output .= ($index ? '' : '') . '<button class="btn btn-primary btn-xs ' . $classes . '"' . $variables['item_attributes'][$delta] . '>' . drupal_render($item) . '</button>';
+    $index++;
+  }
+  $output .= '</div>';
+
+  // Render the top-level div.
+
+  $output = '<div class="' . $variables['classes'] . '"' . $variables['attributes'] . '>' . $output . '</div>';
+
+  return $output;
+}
