e8a4c348ec4ce90f9363ff89f40cb5442e49faa1
Andrew Burns <aburnsni@gmail.com>
2015-09-10 12:13:15 +0100

    Removed extra comments and moved gmap js to inline

--- templates/html--front.tpl.php
+++ templates/html--front.tpl.php
@@ -1,49 +1,4 @@
-<?php
-/**
- * @file
- * Default theme implementation to display the basic html structure of a single
- * Drupal page.
- *
- * Variables:
- * - $css: An array of CSS files for the current page.
- * - $language: (object) The language the site is being displayed in.
- *   $language->language contains its textual representation.
- *   $language->dir contains the language direction. It will either be 'ltr' or
- *   'rtl'.
- * - $rdf_namespaces: All the RDF namespace prefixes used in the HTML document.
- * - $grddl_profile: A GRDDL profile allowing agents to extract the RDF data.
- * - $head_title: A modified version of the page title, for use in the TITLE
- *   tag.
- * - $head_title_array: (array) An associative array containing the string parts
- *   that were used to generate the $head_title variable, already prepared to be
- *   output as TITLE tag. The key/value pairs may contain one or more of the
- *   following, depending on conditions:
- *   - title: The title of the current page, if any.
- *   - name: The name of the site.
- *   - slogan: The slogan of the site, if any, and if there is no title.
- * - $head: Markup for the HEAD section (including meta tags, keyword tags, and
- *   so on).
- * - $styles: Style tags necessary to import all CSS files for the page.
- * - $scripts: Script tags necessary to load the JavaScript files and settings
- *   for the page.
- * - $page_top: Initial markup from any modules that have altered the
- *   page. This variable should always be output first, before all other dynamic
- *   content.
- * - $page: The rendered page content.
- * - $page_bottom: Final closing markup from any modules that have altered the
- *   page. This variable should always be output last, after all other dynamic
- *   content.
- * - $classes String of classes that can be used to style contextually through
- *   CSS.
- *
- * @see bootstrap_preprocess_html()
- * @see template_preprocess()
- * @see template_preprocess_html()
- * @see template_process()
- *
- * @ingroup themeable
- */
-?><!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces;?>>
 <head <?php print $rdf_namespaces; ?>>
   <meta charset="utf-8">
@@ -56,9 +11,8 @@
   <!--[if lt IE 9]>
     <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
   <![endif]-->
-  <?php print $scripts; ?>
   <script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>
-  <script src=<?php print (drupal_get_path('theme', 'fleming') . '/js/gmap-block.js');?>></script>
+  <?php print $scripts; ?>
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
   <div id="content-wrapper">
@@ -69,5 +23,37 @@
     <?php print $page; ?>
     <?php print $page_bottom; ?>
   </div>
+<script>
+var myLatlng = new google.maps.LatLng(54.55663, -5.964576);
+
+function initialize() {
+    "use strict";
+
+    var mapOptions = {
+            zoom: 15,
+            center: myLatlng,
+            draggable: 0,
+            mapTypeControl: 0,
+            zoomControl: 0,
+            scrollwheel: 0,
+            streetViewControl: 0
+        },
+        map = new google.maps.Map(document.getElementById('map-canvas-block'), mapOptions),
+        marker = new google.maps.Marker({
+            icon: 'http://maps.google.com/mapfiles/ms/micons/blue-dot.png',
+            position: myLatlng,
+            map: map,
+            title: 'Fleming Fulton School',
+            url: "/contact_us",
+            animation: google.maps.Animation.DROP
+        });
+    google.maps.event.addListener(marker, 'click', function () {
+        window.location.href = marker.url;
+    });
+
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+</script>
 </body>
 </html>
