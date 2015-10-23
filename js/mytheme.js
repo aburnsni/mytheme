de70cb7a76d50685c53ebe1b34975ff79cc670cd
Andrew Burns <aburnsni@gmail.com>
2015-10-23 10:04:38 +0100

    Fixes for top page margin

--- js/mytheme.js
+++ js/mytheme.js
@@ -3,7 +3,6 @@ jQuery(function ($) {
     "use strict";
 
     function mouseOver() { $(this).find('.dropdown-menu').first().stop(true, true).delay(250).slideDown(); }
-
     function mouseOut() { $(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp(); }
 
     $(window).resize(function () {
@@ -13,10 +12,16 @@ jQuery(function ($) {
             $('body').removeAttr("style");
             //remove auto dropdown from main navbar
             $('.navbar #main-navbar .dropdown').off('mouseenter mouseleave', mouseOver, mouseOut);
+            if (!parseInt($('#main-navbar').height(), 10)) {
+                $('body').attr("style", "padding-top: 0px !important;");
+            }
         } else {
-            var x = "padding-top: " + (parseInt($('#main-navbar').height(), 10) + 10) + "px !important";
-            $('body').attr("style", x);
-
+            if (parseInt($('#main-navbar').height(), 10)) {
+                var x = "padding-top: " + (parseInt($('#main-navbar').height(), 10) + 10) + "px !important";
+                $('body').attr("style", x);
+            } else {
+                $('body').attr("style", "padding-top: 0px !important;");
+            }
             //Set navbar drop down on hover
             $('.navbar #main-navbar .dropdown').hover(mouseOver, mouseOut);
         }
@@ -28,16 +33,19 @@ jQuery(function ($) {
             $('body').removeAttr("style");
             //remove auto dropdown from main navbar
             $('.navbar #main-navbar .dropdown').off('mouseentermouseleave', mouseOver, mouseOut);
-
+            if (!parseInt($('#main-navbar').height(), 10)) {
+                $('body').attr("style", "padding-top: 0px !important;");
+            }
         } else {
-            var x = "padding-top: " + (parseInt($('#main-navbar').height(), 10) + 10) + "px !important";
-            $('body').attr("style", x);
+            if (parseInt($('#main-navbar').height(), 10)) {
+                var x = "padding-top: " + (parseInt($('#main-navbar').height(), 10) + 10) + "px !important";
+                $('body').attr("style", x);
+            } else {
+                $('body').attr("style", "padding-top: 0px !important;");
+            }
 
             //Set navbar drop down on hover
             $('.navbar #main-navbar .dropdown').hover(mouseOver, mouseOut);
         }
     });
-
-
-
 });
